9fe7cbfa9558d708a42b79508a04528efc525dc1
Diego Sousa <diegointeriores11@gmail.com>
2025-12-09 09:57:55 -0300

    feat: refactor StripeIntentPort to use StripeMaker for client initialization

--- app/server/stripe/StripeIntentPort.ts
+++ app/server/stripe/StripeIntentPort.ts
@@ -1,13 +1,17 @@
 import Stripe from 'stripe'
-import { DB, type Product } from '../database/Products'
+import { type Product } from '../database/Products'
 import { Currency } from '~/utils/Currency';
+import { StripeMaker } from './StripeMaker';
 
 export class StripeIntentPort {
     async create(product: Product, currency: string) {
-        const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!)
+        const stripeClient = StripeMaker()
         const paymentIntent = await stripeClient.paymentIntents.create({
             amount: Currency.toStripeAmount(product.price),
             currency: currency.toLowerCase(),
+            metadata: {
+                productId: product.id.toString()
+            }
         });
 
         return paymentIntent
