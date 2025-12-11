8e9e95330c69010c84af7685d9ef6ac987d4215a
Diego Sousa <diegointeriores11@gmail.com>
2025-12-10 22:59:27 -0300

    configure hono

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  basename: "/app", // Now the React Router app will be mounted on /app
   ssr: true,
-  presets: [vercelPreset()],
 } satisfies Config;
