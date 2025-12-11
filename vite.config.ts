f9f009cd25eb530b9c9a08eb62a9775f45a9f128
Diego Sousa <diegointeriores11@gmail.com>
2025-12-10 23:19:12 -0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+        input: "./app/server.ts",
+      }
+      : undefined,
+  },
   plugins: [tailwindcss(), reactRouterHonoServer(), reactRouter(), tsconfigPaths()],
-});
+}));
