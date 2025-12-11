2eba858ea3d085e19d1e22b05a6121e63266b703
Diego Sousa <diegointeriores11@gmail.com>
2025-12-10 22:16:05 -0300

    configure hono

--- app/server.ts
+++ app/server.ts
@@ -1,30 +1,9 @@
 import { Hono } from "hono";
 import { createHonoServer } from "react-router-hono-server/node";
-import { DB } from "./server/database/Database";
-import { StripePurchaseAdapter } from "./server/stripe/StripePurchaseAdapter";
+import { api } from "./server/api";
 
 const app = new Hono()
-
-app.get("/api/purchases", async (c) => {
-    const session = c.req.query("user_session");
-    if (!session) return c.json({ error: "Missing session" }, 400);
-
-    const db = await DB.make();
-    const purchases = await db.getPurchases(session);
-
-    return c.json(purchases);
-});
-
-app.post("/stripe/webhook", async (c) => {
-    const db = await DB.make()
-    const stripeAdapter = await StripePurchaseAdapter.make()
-    const purchase = await stripeAdapter.processPayment(c.req)
-
-    await db.updatePurchase(purchase)
-
-    return c.text("Webhook received");
-})
-
+app.route('/api', api);
 export default createHonoServer({
     app
 })
--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,7 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename: "/app", // Now the React Router app will be mounted on /app
   ssr: true,
   presets: [vercelPreset()],
 } satisfies Config;
