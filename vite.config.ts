3395fccedbd79c57d80fac2c712791f5b536e532
Diego Sousa <diegointeriores11@gmail.com>
2025-12-10 21:45:30 -0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouterHonoServer(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouterHonoServer(), reactRouter(), tsconfigPaths(), vercelPreset()],
 });
